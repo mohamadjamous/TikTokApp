753bd7a119cec509d656ccd02623eaa74624a7e3
Mohamad Jamous <mohamadjamous@apples-MacBook-Pro.local>
2025-10-08 15:02:07 +0300

    first commit

---
+++ TikTokApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TikTokApp
+//
+//  Created by Mohamad Jamous on 10/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TikTokApp/TikTokAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokAppApp.swift
+//  TikTokApp
+//
+//  Created by Mohamad Jamous on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TikTokAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
